0697113a2bac55d73687bf7b255efaa958e7b3c0
NasgulNexus <moctegor@gmail.com>
2016-11-24 17:08:03 +0300

    first

---
+++ DistrictDict.dpr
@@ -0,0 +1,13 @@
+program DistrictDict;
+
+uses
+  Forms,
+  Unit1 in '..\MyProject\Unit1.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
